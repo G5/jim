ad3c61b78e154b7ebee3c48ee25c1028dec89b30
Don Petersen <don@donpetersen.net>
2014-03-14 22:50:42 +0000

    Show the details of an environment check.

--- spec/features/feature_list_spec.rb
+++ spec/features/feature_list_spec.rb
@@ -31,10 +31,12 @@ describe "Feature list" do
       within(".enablements") do
         expect(page).to have_content("Environment variable 'SHELL'")
         expect(page).to have_content(ENV["SHELL"])
+        expect(page).to have_content("/sh/")
         expect(page).to have_content("Example: /bin/fish")
 
         expect(page).to have_content("Environment variable 'USER'")
         expect(page).to have_no_content(ENV["USER"])
+        expect(page).to have_content("not blank")
         expect(page).to have_content("redacted")
       end
 

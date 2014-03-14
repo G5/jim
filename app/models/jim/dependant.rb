f640852b3a367ac15b6e92cc563672d8c6d46d63
Don Petersen <don@donpetersen.net>
2014-03-14 22:16:14 +0000

    Allow using class constants as dependent names.
    This will purposely allow your code to blow up if you removed the code
    but forgot to remove the dependant documentation.

--- app/models/jim/dependant.rb
+++ app/models/jim/dependant.rb
@@ -2,7 +2,7 @@ class Jim::Dependant
   attr_reader :name
 
   def initialize(name, description)
-    @name, @description = name, description
+    @name, @description = name.to_s, description
   end
 
   def description
--- spec/models/jim/dependant_spec.rb
+++ spec/models/jim/dependant_spec.rb
@@ -3,7 +3,19 @@ require 'spec_helper'
 describe Jim::Dependant do
   subject { Jim::Dependant.new("Name", "Description") }
 
-  its(:name) { should eq("Name") }
+  describe "#name" do
+    context "passed a string" do
+      its(:name) { should eq("Name") }
+    end
+
+    context "passed some other object" do
+      subject { Jim::Dependant.new(Jim::Dependant, "Description").name }
+
+      it "makes it a string" do
+        should eq("Jim::Dependant")
+      end
+    end
+  end
 
   describe "#description" do
     its(:description) { should include("Description") }
